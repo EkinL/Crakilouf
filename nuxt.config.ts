ab9ebdcb91610db5d6f70b33ccaee501548dfaba
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-10-08 16:57:10 +0200

    Add gTag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+
+  modules: ['nuxt-gtag'],
+
+  gtag: {
+    id: 'G-NYN47FGJ5Z',
   },
 })
