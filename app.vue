fbf59cac18e1ea38c8a50523224de67bd63749e1
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-10-08 15:45:57 +0200

    Ajout du SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Crakilouf - Home',
+  ogTitle: 'Crakilouf - Home',
+  description: 'Crakilouf news and updates - all in one place!',
+  ogDescription: 'Crakilouf news and updates - all in one place!',
+  twitterCard: 'summary_large_image',
+})
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
