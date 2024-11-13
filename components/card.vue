9df06cac5ad40598172bd6eca45deb828b838bc8
Zeyoman <alex10pump@gmail.com>
2024-11-13 12:22:04 +0100

    delete style

--- components/card.vue
+++ components/card.vue
@@ -1,36 +1,3 @@
 <template>
     <h2>Hello World !</h2>
 </template>
-
-<style scoped>
-#card-container {
-    border: 1px solid #ccc;
-    padding: 16px;
-    max-width: 600px;
-    margin: auto;
-}
-#card-image img {
-    width: 100%;
-    height: auto;
-}
-#card-info {
-    padding: 16px;
-}
-#card-title h1 {
-    margin: 0 0 8px;
-    font-size: 24px;
-}
-#card-post-info span {
-    display: inline-block;
-    margin-right: 8px;
-    color: #757575;
-}
-#card-description p {
-    margin: 16px 0;
-}
-#card-signature p {
-    text-align: right;
-    font-style: italic;
-    color: #9e9e9e;
-}
-</style>
