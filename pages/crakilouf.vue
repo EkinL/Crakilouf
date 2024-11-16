a847c745ab5ceaeca8854eae9059088f595d15a1
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-11-16 20:50:46 +0100

    test

--- pages/crakilouf.vue
+++ pages/crakilouf.vue
@@ -2,7 +2,7 @@
     <div>
         <h1 class="under">Crakilouf</h1>
         <NuxtLink to="/">
-            home page test
+            home page test v1
         </NuxtLink>
         <CrakiloufCreation />
     </div> 
